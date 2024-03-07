9aba146f5d0ac70c356669c777944b73ec454602
ydwu4 <ydwu2014@gmail.com>
2024-03-07 14:02:49 -0800

    Add torch._library.impl_abstract_method
    ghstack-source-id: ed9ed3478a1862d8c22bcd3418460106d729bbd0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121155

--- test/export/test_torchbind.py
+++ test/export/test_torchbind.py
@@ -216,8 +216,7 @@ def forward(self, arg0_1, arg1_1):
 
         m = MyModule()
         input = torch.ones(2, 3)
-        with enable_torchbind_tracing():
-            ep = torch.export.export(m, (input,), strict=False)
+        ep = self._test_export_same_as_eager(m, (input,), strict=False)
 
         unlifted = ep.module()
         self.assertExpectedInline(
@@ -245,9 +244,6 @@ def forward(self, attr, arg0_1):
 
 @skipIfTorchDynamo("torchbind not supported with dynamo yet")
 class TestImplAbstractClass(TestCase):
-    def tearDown(self):
-        torch._library.abstract_impl_class.global_abstract_class_registry.clear()
-
     def test_impl_abstract_class_no_torch_bind_class(self):
         with self.assertRaisesRegex(RuntimeError, "Tried to instantiate class"):
 
@@ -266,9 +262,7 @@ class TestImplAbstractClass(TestCase):
                     pass
 
     def test_impl_abstract_class_from_real_not_classmethod(self):
-        with self.assertRaisesRegex(
-            RuntimeError, "must define a classmethod from_real"
-        ):
+        with self.assertRaisesRegex(RuntimeError, "from_real have to be a classmethod"):
 
             @torch._library.impl_abstract_class("_TorchScriptTesting::_Foo")
             class FakeFoo:
@@ -292,6 +286,9 @@ class TestImplAbstractClass(TestCase):
                 return cls(x, y)
 
         torch._library.impl_abstract_class("_TorchScriptTesting::_Foo", FakeFoo)
+        torch._library.abstract_impl_class.deregister_abstract_impl(
+            "_TorchScriptTesting::_Foo"
+        )
 
     def test_impl_abstract_class_duplicate_registration(self):
         @torch._library.impl_abstract_class("_TorchScriptTesting::_Foo")
@@ -307,6 +304,9 @@ class TestImplAbstractClass(TestCase):
 
         with self.assertRaisesRegex(RuntimeError, "already registered"):
             torch._library.impl_abstract_class("_TorchScriptTesting::_Foo", FakeFoo)
+        torch._library.abstract_impl_class.deregister_abstract_impl(
+            "_TorchScriptTesting::_Foo"
+        )
 
 
 if __name__ == "__main__":
--- torch/_subclasses/fake_tensor.py
+++ torch/_subclasses/fake_tensor.py
@@ -811,6 +811,37 @@ class DispatchCacheInfo:
 # memory should not significantly increase.
 
 
+def _fakify_script_object(x):
+    full_qualname = x._type().qualified_name()
+    splits = full_qualname.split(".")
+    assert len(splits) == 5, breakpoint()
+    _torch, torch_ns, classes, ns, class_name = splits
+
+    fake_class = torch._library.abstract_impl_class.find_fake_impl(full_qualname)
+    if fake_class is None:
+        raise RuntimeError(
+            f" ScriptObject's {full_qualname} haven't registered a fake class."
+            f" Please use impl_abstract_class({ns}::{class_name}) to annotate a fake class for the script obj."
+            f" Specifically, create a python class that implements a fake version for all the methods"
+            f" that're used in the program and put annotated class in the program e.g. after loading the library."
+            f" The fake methods can be written in the same way as a meta kernel for an operator but need to also"
+            f" simulate the object's states when necessary. Be sure to add a from_real classmethod to enable creating"
+            f" a fake obj from a real one."
+        )
+    if not hasattr(fake_class, "from_real"):
+        raise RuntimeError(
+            f"ScriptObject {full_qualname}'s corresponding fake_class {fake_class}"
+            f" doesn't implement a from_real classmethod. Please add it to the fake class."
+        )
+    return fake_class.from_real(x)
+
+
+def _maybe_fakify_script_object(args, kwargs):
+    return pytree.tree_map_only(
+        torch.ScriptObject, lambda x: _fakify_script_object(x), (args, kwargs)
+    )
+
+
 class FakeTensorMode(TorchDispatchMode):
     cache: Dict[_DispatchCacheKey, _DispatchCacheEntry] = {}
     cache_hits: int = 0
@@ -1231,36 +1262,13 @@ class FakeTensorMode(TorchDispatchMode):
             # NOTE: incr is intentionally unused for a RAII pattern
             incr = IncrementRecursionCount()
 
-        def _fakify_script_object(x):
-            # NOTE: Skip fakifying inputs to torch.ops.profiler because they only have c++ implementation
-            # and are performance critical. The suggested workaround will cause non-eligible overhead.
-            if func in {
-                torch.ops.profiler._record_function_enter_new,
-                torch.ops.profiler._record_function_exit._RecordFunction,
-            }:
-                return x
-
-            full_qualname = x._type().qualified_name()
-            fake_class = torch._library.abstract_impl_class.find_fake_impl(
-                full_qualname
-            )
-            if fake_class is None:
-                raise RuntimeError(
-                    f" ScriptObject's {full_qualname} haven't registered a fake class. If {func} is supposed "
-                    f" to be exported as a node in graph and preserve the script obj as input to the node, "
-                    f" please use impl_abstract_class to register a fake class for the script obj. Otherwise,"
-                    f" consider disabling fake modes for the operator by callig it with ctx manager maybe_disable_fake_tensor_mode."
-                )
-            if not hasattr(fake_class, "from_real"):
-                raise RuntimeError(
-                    f"ScriptObject {full_qualname}'s corresponding fake_class {fake_class}"
-                    f" doesn't implement a from_real classmethod. Please add it to the fake class."
-                )
-            return fake_class.from_real(x)
-
-        args, kwargs = pytree.tree_map_only(
-            torch.ScriptObject, lambda x: _fakify_script_object(x), (args, kwargs)
-        )
+        # NOTE: Skip fakifying inputs to torch.ops.profiler because they only have c++ implementation
+        # and are performance critical. The suggested workaround will cause non-eligible overhead.
+        if func not in {
+            torch.ops.profiler._record_function_enter_new,
+            torch.ops.profiler._record_function_exit._RecordFunction,
+        }:
+            args, kwargs = _maybe_fakify_script_object(args, kwargs)
 
         # Some attribute queries that can be serviced directly
         # See Note [is_coalesced is dispatched]
